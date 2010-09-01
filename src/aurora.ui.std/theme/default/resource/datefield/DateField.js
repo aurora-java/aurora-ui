47007a74a51e5f2a0ca8c6bb7f8eb1e78f854523
znjqolf <znjqolf@126.com>
2010-09-01 08:39:30 +0000

    解决firefor的问题

--- src/aurora.ui.std/theme/default/resource/datefield/DateField.js
+++ src/aurora.ui.std/theme/default/resource/datefield/DateField.js
@@ -65,7 +65,7 @@ $A.DateField = Ext.extend($A.Component, {
     },
     mouseOver: function(e){
     	if(this.overTd) Ext.fly(this.overTd).removeClass('dateover');
-    	if(Ext.fly(e.target).hasClass('item-day') && e.target.date != 0){
+    	if(Ext.fly(e.target).hasClass('item-day') && Ext.fly(e.target).getAttribute('_date') != '0'){
     		this.overTd = e.target; 
     		Ext.fly(this.overTd).addClass('dateover');
     	}
@@ -76,10 +76,10 @@ $A.DateField = Ext.extend($A.Component, {
     		
     	}else{
     		if(this.selectedDay) Ext.fly(this.selectedDay).removeClass('onSelect');
-    		if(Ext.fly(e.target).hasClass('item-day') && e.target.date != 0){
+    		if(Ext.fly(e.target).hasClass('item-day') && Ext.fly(e.target).getAttribute('_date') != '0'){
 	    		this.selectedDay = e.target; 
 	    		this.onSelectDay(this.selectedDay);
-	    		this.fireEvent('select', this, this.selectedDay.date);
+	    		this.fireEvent('select', this, new Date(parseInt(Ext.fly(e.target).getAttribute('_date'))));
 	    	}
     	}
     },
@@ -170,14 +170,14 @@ $A.DateField = Ext.extend($A.Component, {
 				var cell = document.createElement("td"); 
 				cell.className = "item-day";
 				cell.innerHTML = "&nbsp;";
-				Ext.fly(cell).set({'date':0});
+				Ext.fly(cell).set({'_date':'0'});
 				if(arr.length){
 					var d = arr.shift();
 					if(d){
 						cell.innerHTML = d;
 						this.days[d] = cell;
 						var on = new Date(this.year, this.month - 1, d);
-						Ext.fly(cell).set({'date':on});
+						Ext.fly(cell).set({'_date':''+on.getTime()});
 						//判断是否今日
 						this.isSame(on, new Date()) && this.onToday(cell);
 						//判断是否选择日期
