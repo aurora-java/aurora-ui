0d7b17b3013ca18d29c6fdba94928431149608af
njq.niu@gmail.com <njq.niu@gmail.com@c3805726-bc34-11dd-8164-2ff6f70dce53>
2010-10-29 07:29:59 +0000

    修改了lov的blur

--- trunk/AuroraUI/src/aurora.ui.std/theme/default/resource/core/Aurora-all.js
+++ trunk/AuroraUI/src/aurora.ui.std/theme/default/resource/core/Aurora-all.js
@@ -4841,6 +4841,7 @@ $A.Window = Ext.extend($A.Component,{
         this.shadow.remove();
         var sf = this;
         setTimeout(function(){
+        	debugger
         	for(var key in sf.cmps){
         		var cmp = sf.cmps[key];
         		if(cmp.destroy){
@@ -4958,8 +4959,9 @@ $A.showOkWindow = function(title, msg, width, height,callback){
     			var btn = $("aurora-msg-yes");
                 cmp.cmps['aurora-msg-yes'] = btn;
                 btn.on('click',function(){
-                if(callback)callback.call(this,cmp)
-                cmp.close()});
+                    if(callback)callback.call(this,cmp);
+                    cmp.close()
+                });
                 btn.focus();
 			});
 		}
@@ -5087,16 +5089,16 @@ $A.Lov = Ext.extend($A.TextField,{
 		}else if(!Ext.isEmpty(this.lovmodel)){
 			url = this.context + 'autocrud/'+this.lovmodel+'/query?pagesize=1&pagenum=1&_fetchall=false&_autocount=false&'+ Ext.urlEncode(this.getLovPara());
 		}
+		var record = this.record;
 		var p = {};
 		var mapping = this.getMapping();
 		for(var i=0;i<mapping.length;i++){
-			var map = mapping[i];
+			var map = mapping[i];			
 			if(this.binder.name == map.to){
 				p[map.from]=v;
-				break;
 			}
+			record.set(map.to,'');			
 		}
-		var record = this.record;
 		$A.slideBarEnable = $A.SideBar.enable;
         $A.SideBar.enable = false;
         this.setRawValue('正在查询...')
@@ -5118,15 +5120,16 @@ $A.Lov = Ext.extend($A.TextField,{
 		$A.SideBar.enable = $A.slideBarEnable;
 //		$A.showErrorMessage('错误', res.error.message);
 	},
-//	onBlur : function(e){
+	onBlur : function(e){
 //        if(this.isEventFromComponent(e.target)) return;
 //        var sf = this;
 //        setTimeout(function(){
 //            if(!this.isWinOpen){
 //            }
 //        })
-//        $A.Lov.superclass.onBlur.call(this,e);
-//    },
+		if(!fetching)
+        $A.Lov.superclass.onBlur.call(this,e);
+    },
 	showLovWindow : function(e){
 		e.stopEvent();
 		if(this.fetching||this.isWinOpen||this.readonly) return;
