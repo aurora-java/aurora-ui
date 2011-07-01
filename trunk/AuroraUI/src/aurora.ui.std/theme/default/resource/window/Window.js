5122db37ca8f1b8aa7658745bd952b288c8fe8b0
Hugh.HZ.Wu <Hugh.HZ.Wu@c3805726-bc34-11dd-8164-2ff6f70dce53>
2011-07-01 08:42:35 +0000

    * 修正了Chrome下Window移动时的BUG

--- trunk/AuroraUI/src/aurora.ui.std/theme/default/resource/window/Window.js
+++ trunk/AuroraUI/src/aurora.ui.std/theme/default/resource/window/Window.js
@@ -249,8 +249,8 @@ $A.Window = Ext.extend($A.Component,{
     },
     onMouseMove : function(e){
     	e.stopEvent();
-    	var sl = document[Ext.isStrict?'documentElement':'body'].scrollLeft;
-    	var st = document[Ext.isStrict?'documentElement':'body'].scrollTop;
+    	var sl = document[Ext.isStrict&&!Ext.isChrome?'documentElement':'body'].scrollLeft;
+    	var st = document[Ext.isStrict&&!Ext.isChrome?'documentElement':'body'].scrollTop;
     	var sw = sl + this.screenWidth;
     	var sh = st + this.screenHeight;
     	var tx = e.getPageX()+this.relativeX;
