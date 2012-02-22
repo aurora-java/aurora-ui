a4a88715511b54a3ed24ad5473185dd4dbb67f44
Hugh.HZ.Wu <Hugh.HZ.Wu@c3805726-bc34-11dd-8164-2ff6f70dce53>
2012-02-22 05:50:04 +0000

    * 组件DateTimePicker增加hour,minute,second属性

--- trunk/AuroraUI/src/aurora.ui.std/theme/default/resource/datefield/DatePicker.js
+++ trunk/AuroraUI/src/aurora.ui.std/theme/default/resource/datefield/DatePicker.js
@@ -275,9 +275,9 @@ $A.DatePicker = Ext.extend($A.TriggerField,{
 			this.selectDay=new Date(date);
 		}else {
 			date=new Date();
-			date.setHours(0);
-			date.setMinutes(0);
-			date.setSeconds(0);
+			date.setHours(this.hour||0);
+			date.setMinutes(this.minute||0);
+			date.setSeconds(this.second||0);
 			date.setMilliseconds(0);
 		}
 		this.draw(date);
